262ab1e963720cefb5f909b9e5b07c2fe8a801fc
GerSay <kryt.master@yandex.ru>
2022-03-22 11:27:04 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/labsForOP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labsForOP.dir/main.cpp.obj"
+  "labsForOP.exe"
+  "labsForOP.exe.manifest"
+  "labsForOP.pdb"
+  "liblabsForOP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labsForOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+#define TIME_TEST(testCode, message) {\
+    clock_t start_time = clock(); \
+    testCode \
+    clock_t end_time = clock();       \
+    clock_t sort_time = end_time - start_time; \
+    std::clog << message << ": " \
+              << (double) sort_time / CLOCKS_PER_SEC << std::endl; \
+}
+
+int main()
+{
+    const char *filename = "tmp.txt";
+    std::ofstream file(filename);
+
+    TIME_TEST({
+        for (size_t i = 0; i < 1000000; i++)
+            file << 'a' << std::endl;
+    }, "Often buffer reset");
+
+    TIME_TEST({
+        for (size_t i = 0; i < 1000000; i++)
+            file << 'a' << '\n';
+    }, "Buffer opt");
+
+    file.close();
+    std::remove(filename);
+
+    return 0;
+}*/
+
